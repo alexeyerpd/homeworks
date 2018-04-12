2c4efb0a107e159d02f8e41f3a1cb2bd0855ffd6
alexeyerpd <alexeyerpd@gmail.com>
2018-04-12 20:15:08 +0300

    update

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -11,57 +11,18 @@ const wallX = wall.clientWidth;
 const wallY = wall.clientHeight;
 const ctx = wall.getContext('2d');
 
-function nextPoint1(x, y, time) { // функция времени 1
+function nextPoint1(x, y, time = Date.now()) { // функция времени 1
   return {
-    x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
-    y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
+    x: this.x + Math.sin((50 + this.x + (time / 10)) / 100) * 3,
+    y: this.y + Math.sin((45 + this.x + (time / 10)) / 100) * 4
   };
 }
 
-function nextPoint2(x, y, time) { // функция времени 2
+function nextPoint2(x, y, time = Date.now()) { // функция времени 2
   return {
-    x: x + Math.sin((x + (time / 10)) / 100) * 5,
-    y: y + Math.sin((10 + x + (time / 10)) / 100) * 2
+    x: this.x + Math.sin((this.x + (time / 10)) / 100) * 5,
+    y: this.y + Math.sin((10 + this.x + (time / 10)) / 100) * 2
   }
-}
-
-function circle(x, y) {
-  ctx.beginPath();
-
-  const size = random(0.1, 0.6);
-
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
-  ctx.strokeStyle = '#fff';
-  ctx.lineWidth = 5 * size;
-  ctx.arc(x, y, (12 * size), 0, 2 * PI);
-  ctx.stroke()
-}
-
-function cross(x, y) {
-  ctx.beginPath();
-  ctx.save();
-  ctx.translate(x, y);
-  ctx.rotate(0.017 * random(0, 360))
-  ctx.strokeStyle = '#fff'
-  const size = random(0.1, 0.6) * 20;
-  const centerX = 0;
-  const centerY = 0;
-
-  ctx.moveTo(centerX, centerY)
-  ctx.lineTo(centerX + size, centerY);
-
-  ctx.moveTo(centerX, centerY)
-  ctx.lineTo( centerX - size, centerY);
-
-  ctx.moveTo(centerX, centerY)
-  ctx.lineTo( centerX, centerY + size);
-
-  ctx.moveTo(centerX, centerY)
-  ctx.lineTo(centerX, centerY - size);
-
-  ctx.stroke();
-  ctx.restore();
 }
 
 function random(min, max) {
@@ -69,38 +30,93 @@ function random(min, max) {
 }
 
 
+class Circle {
+  constructor(x, y, size = random(0.1, 0.6)) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+
+  create({x, y}) {
+    ctx.beginPath();
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#fff';
+    ctx.lineWidth = 5 * this.size;
+
+    ctx.arc(x, y, (this.size * 12), 0, 2 * PI);
+    ctx.stroke()
+  }
+}
+
+class Cross {
+  constructor(x, y, speed = random(-0.2, 0,2), size = random(0.1, 0.6) * 20) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.degree = 0;
+    this.size = size;
+  }
+
+  create() {
+    ctx.beginPath();
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    this.checkRotate();
+
+    ctx.rotate(this.degree);
+    ctx.strokeStyle = '#fff';
+
+    const centerX = 0;
+    const centerY = 0;
+
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(centerX + this.size, centerY);
+
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo( centerX - this.size, centerY);
+
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo( centerX, centerY + this.size);
+
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(centerX, centerY - this.size);
+
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  checkRotate() {
+    if (this.degree > (2 * PI)) {
+      this.degree = 0;
+    }
+
+    this.degree += this.speed;
+  }
+}
+
 const quantity = Math.round(random(50, 200));
 const arrayCircles = [];
 const arrayCross = [];
 
 for (let i = 0; i < (quantity / 2); i++) {
-  let {x, y} = [nextPoint1(random(0, wallX), random(0, wallY), Date.now()), nextPoint2(random(0, wallX), random(0, wallY), Date.now())][Math.round(Math.random())];
-
-  arrayCircles.push({x, y})
+  const circle = new Circle(random(0, wallX),random(0, wallY));
+  circle.animations = [nextPoint1, nextPoint2][Math.round(Math.random())];
+  arrayCircles.push(circle)
 }
 
 for (let i = 0; i < (quantity / 2); i++) {
-  let {x, y} = [nextPoint1(random(0, wallX), random(0, wallY), Date.now()), nextPoint2(random(0, wallX), random(0, wallY), Date.now())][Math.round(Math.random())];
+  const cross = new Cross(random(0, wallX),random(0, wallY));
+  cross.animations = [nextPoint1, nextPoint2][Math.round(Math.random())];
 
-  arrayCross.push({x, y})
+  arrayCross.push(cross)
 }
 
+const objects =  arrayCircles.concat(arrayCross);
 
-arrayCircles.forEach(({x, y}, ind) => {
-  setInterval(() => {
-    ctx.clearRect(x - 6, y - 6, x + 6, y + 6);
-    circle(x, y);
-  }, 20)
-})
-
-arrayCross.forEach(({x, y}, ind) => {
-  setInterval(() => {
-    ctx.clearRect(x - 8, y - 8, x + 8, y + 8);
-    cross(x, y);
-  }, 20)
-
-})
-
-
-
-
+setInterval(() => {
+  ctx.clearRect(0,0, wall.width, wall.height);
+  objects.forEach((obj, ind) => {
+    obj.create(obj.animations())
+  })
+}, 50);
